89dcbca0c08cbbedfc4caa5bd2997e1d71d7d54e
Grant Phillips <Grant Phillips>
2017-06-28 21:27:49 -0400

    Mobile menu fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  public collapseMenu = true;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
